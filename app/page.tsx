d1aeeb657fd5dd11b801cb1cfcb6a66e86f5a44a
PARK-YOUNG-GYU <0q@colosseum.kr>
2024-04-17 09:03:19 +0900

    rendering 개선

--- app/page.tsx
+++ app/page.tsx
@@ -27,12 +27,12 @@ function App() {
   const searchParams = useSearchParams();
   const [shapes, setShapes] = useState<Shape[]>(handleGetQuery(searchParams.get('data') as string) || []);
   const [newShape, setNewShape] = useState<Shape | null>(null); // 새로운 도형
-  const [isDrawing, setIsDrawing] = useState<boolean>(false); // 그리는 중
+  const isDrawing = useRef(false);
+  const isMoving = useRef(false);
   const [drawMode, setDrawMode] = useState<'rectangle' | 'circle'>('rectangle'); // 도형 타입
   const id = useId(); // id 생성
   const canvasRef = useRef<HTMLDivElement>(null);
   const [selectedShape, setSelectedShape] = useState<Shape | null>(null);
-  const [isMoving, setIsMoving] = useState<boolean>(false);
 
   // MouseEvent의 e.nativeEvent 에서 offset 값을 가져올 경우 좌표가 정확하지 않는 이슈가 있어서 ref로 다시 계산
   const getOffset = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -50,7 +50,7 @@ function App() {
   // 마우스클릭 시점의 좌표를 새로운 도형 객체로 만든다.
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const { offsetX, offsetY } = getOffset(e);
-    setIsDrawing(true);
+    isDrawing.current = true;
     setNewShape({
       id: id + shapes.length,
       type: drawMode,
@@ -65,7 +65,7 @@ function App() {
 
   // 마우스를 움직이는 동안 새로운 도형 객체를 업데이트 한다.
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!isDrawing || !newShape) return;
+    if (!isDrawing.current || !newShape) return;
     const { offsetX, offsetY } = getOffset(e);
 
     const width = offsetX - newShape.startX;
@@ -82,14 +82,12 @@ function App() {
 
   // 마우스 클릭을 떼면 새로운 도형 객체를 기존 도형 Array로 추가한다.
   const handleMouseUp = () => {
-    setIsDrawing(false);
-
-    // 새로운 객체 생성시에 width, height 이 0일때 점으로 도형 생성 방지
+    isDrawing.current = false;
+    // // 새로운 객체 생성시에 width, height 이 0일때 점으로 도형 생성 방지
     if (!newShape || newShape.width < 1 || newShape.height < 1) {
       setNewShape(null);
       return false;
     }
-
     setShapes([...shapes, newShape]);
     setNewShape(null);
   };
@@ -102,19 +100,19 @@ function App() {
   // 도형 클릭해서 선택 기능
   const handleSelectedMouseDown = (shape: Shape) => {
     setSelectedShape(shape);
-    setIsMoving(true);
+    isMoving.current = true;
   };
 
   // 도형 선택 후 해당 좌표값 업데이트
   const handleSelectedMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!isMoving || !selectedShape) return;
+    if (!isMoving.current || !selectedShape) return;
     const { offsetX, offsetY } = getOffset(e);
     setShapes(shapes.map((d) => (d.id === selectedShape?.id ? { ...d, left: offsetX - d.width / 2, top: offsetY - d.height / 2 } : d)));
   };
 
   // 도형 이동 종료
   const handleSelectedMouseUp = () => {
-    setIsMoving(false);
+    isMoving.current = false;
   };
 
   // 캔버스 모두 삭제
